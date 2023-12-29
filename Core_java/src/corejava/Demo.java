f16ca9124d69eef4c69f32a196e726125adae879
swaraj11 <swarajghadage111@gmail.com>
2023-12-29 09:58:11 +0530

    core java code

---
+++ Core_java/src/corejava/Demo.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class Demo 
+{
+
+	public static void main(String[] args)
+	{
+		String a="Hello Java";
+		int id=10;
+		float per=89.66f;
+		
+		System.out.println(per);
+		System.out.println(id);
+		System.out.println(a);
+
+	}
+
+}
